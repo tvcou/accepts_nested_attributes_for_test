33f307e95cb40bbb808443c05bc4fb0aadd2df49
tvcou <tvcou.zyx77@gmail.com>
2015-06-13 03:46:09 +0000

    "vi app/models/address.rb"

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ActiveRecord::Base
   belongs_to :user
   # モデルを保存できなくなるのでpresence: trueは記載しない
-  validates :user_id, presence: true
+  # validates :user_id, presence: true
   # or validates :user, presence: true
 end
