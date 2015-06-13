07827211cb3f8435b9bce8748854f5b16ba06faa
tvcou <tvcou.zyx77@gmail.com>
2015-06-13 03:58:22 +0000

    "add validates"

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,13 @@
 class Address < ActiveRecord::Base
   belongs_to :user
+
   # モデルを保存できなくなるのでpresence: trueは記載しない
   # validates :user_id, presence: true
   # or validates :user, presence: true
+
+  # presence: true 各項目が存在しない場合バリデーションエラーになる
+  validates :zipcode,  presence: true
+  validates :city,     presence: true
+  validates :street,   presence: true
+  validates :tel,      presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :addresses
   accepts_nested_attributes_for :addresses, allow_destroy: true
+
+  # presence: true 各項目が存在しない場合バリデーションエラーになる
+  validates :username,  presence: true
+  validates :age,       presence: true
 end
